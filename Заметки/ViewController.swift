5f6f9b0b8c0369dd4e8e22c93cd20bdc6157d956
AlenaBelenets <alenaharenkova@gmail.com>
2023-01-03 17:27:18 +0300

    Initial Commit

---
+++ Заметки/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Заметки
+//
+//  Created by Alena Belenets on 03.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
